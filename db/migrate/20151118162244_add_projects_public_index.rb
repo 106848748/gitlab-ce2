1637de85f7edd185d4035d91e0e72e621f9cbeae
Rémy Coutable <remy@rymai.me>
2017-03-17 17:24:33 +0100

    Make many migrations rollbackable
    Don't try to create the index if it already exist. It's possible that a
    previous rollback did not remove the index since the index is not
    dropped on MySQL because MySQL would also drop the foreign key in that
    case, see f67d8eb1da.
    
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- db/migrate/20151118162244_add_projects_public_index.rb
+++ db/migrate/20151118162244_add_projects_public_index.rb
@@ -1,6 +1,13 @@
 # rubocop:disable all
 class AddProjectsPublicIndex < ActiveRecord::Migration
-  def change
+  DOWNTIME = false
+
+  def up
     add_index :namespaces, :public
+  end
+
+  def down
+    # This one is removed in RemovePublicFromNamespace
+    remove_index :namespaces, :public if index_exists?(:namespaces, :public)
   end
 end
