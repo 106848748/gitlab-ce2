13354881fc5d4196928999d8285d50c4747ef81f
http://jneen.net/ <jneen@jneen.net>
2017-04-13 10:58:01 -0700

    move the build overrides to CommitStatusPolicy

--- app/policies/ci/build_policy.rb
+++ app/policies/ci/build_policy.rb
@@ -1,7 +1,4 @@
 module Ci
   class BuildPolicy < CommitStatusPolicy
-    %w[read create update admin].each do |action|
-      rule { ~can?(:"#{action}_build") }.prevent :"#{action}_commit_status"
-    end
   end
 end
