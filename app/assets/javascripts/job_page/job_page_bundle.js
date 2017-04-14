cfd96fb6a080147e702e2ed91bfc247b78ab7b87
Bryce Johnson <bryce@gitlab.com>
2017-04-14 01:53:34 -0400

    Pass real data to job page header.

--- app/assets/javascripts/job_page/job_page_bundle.js
+++ app/assets/javascripts/job_page/job_page_bundle.js
@@ -13,42 +13,19 @@ import Vue from 'vue';
 // - List what should be included in specs
 const CiStatusBadge = {
   props: {
-    ciStatus: { type: String, required: true },
+    ciStatusText: { type: String, required: true },
+    ciStatusTitleText: { type: String, required: true },
   },
-  template: `<div> {{ ciStatus }} </div>`,
+  template: `<div> {{ ciStatusText }} and title: {{ ciStatusTitleText }} </div>`,
 };
 
 window.gl.jobPageData = {};
 
-Object.assign(gl.jobPageData, {
-  job_id: '123432345',
-  ci_status: 'failed',
-  ci_status_title: 'The build failed.',
-  user: {
-    profileUrl: 'http://profile-url.com',
-    avatarUrl: 'http://avatar-url.com',
-    name: 'My User Name',
-  },
-  new_issue_url: 'http://new-issue.com',
-  retry_job_url: 'http://retry-job.com',
-  triggered_at: new Date(),
-});
-
 class JobPageStore {
   constructor(data) {
     this.sidebarState = {};
-    this.pipeline = data.pipeline;
-    this.jobId = data.job_id;
-    this.ciStatus = data.ci_status;
-    this.ciStatusTitle = data.ci_status_title;
-    this.user = data.user;
-    this.newIssueUrl = data.new_issue_url;
-    this.retryJobUrl = data.retry_job_url;
-    this.triggeredAt = data.triggered_at;
-    // user can retry
-    // build failed
+    Object.assign(this, data);
     // build is retryable
-    // user can create issue
   }
 }
 
@@ -56,11 +33,15 @@ const JobPageHeader = {
   props: {
     retryJobUrl: { type: String, required: false },
     newIssueUrl: { type: String, required: false },
-    ciStatus: { type: String, required: true },
-    ciStatusTitle: { type: String, required: true },
-    jobId: { type: String, required: true },
-    user: { type: Object, required: false },
-    triggeredAt: { type: Date, required: true },
+    ciStatusText: { type: String, required: true },
+    ciStatusTitleText: { type: String, required: true },
+    jobId: { type: Number, required: true },
+    jobOwner: { type: Object, required: false },
+    jobFailed: { type: Boolean, required: true },
+    jobRetryable: { type: Boolean, required: true },
+    jobTriggeredAt: { type: String, required: true },
+    userCanRetryJob: { type: Boolean, required: true },
+    userCanCreateIssue: { type: Boolean, required: true},
   },
   components: {
     'ci-status-badge': CiStatusBadge,
@@ -68,19 +49,19 @@ const JobPageHeader = {
   template: `
     <div class="content-block build-header top-area">
       <div class="header-content">
-        <ci-status-badge :ci-status='ciStatus' :ci-status-title='ciStatusTitle'></ci-status-badge>
+        <ci-status-badge :ci-status-text='ciStatusText' :ci-status-title-text='ciStatusTitleText'></ci-status-badge>
         Job
         <strong class="js-build-id"> #{{jobId}} </strong>
         triggered
-        <span> {{ triggeredAt }} </span>
+        <span> {{ jobTriggeredAt }} </span>
         <span>
           by
-          <a v-once :href='user.profileUrl'> {{ user.name }} </a>
-          <img v-once :src='user.avatarUrl'/>
+          <a v-once :href='jobOwner.profileUrl'> {{ jobOwner.name }} </a>
+          <img v-once :src='jobOwner.avatarUrl'/>
         </span>
         <div class="nav-controls">
-          <a class="btn btn-new btn-inverted" v-once :href='newIssueUrl' > New Issue </a>
-          <a class="btn btn-inverted-secondary" v-once :href='retryJobUrl' rel="nofollow"> Retry job </a>
+          <a class="btn btn-new btn-inverted" v-if='userCanCreateIssue' :href='newIssueUrl' > New Issue </a>
+          <a class="btn btn-inverted-secondary" v-if='userCanRetryJob && jobFailed' :href='retryJobUrl' rel="nofollow"> Retry job </a>
           <button class='btn btn-default pull-right visible-xs-block visible-sm-block build-gutter-toggle js-sidebar-build-toggle' role='button' type='button'>
             <i class='fa fa-angle-double-left'></i>
           </button>
@@ -95,8 +76,7 @@ const JobDetailsSidebar = {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  const jobPageData = window.gl.jobPageData;
-
+  const jobPageData = $('#job-page-app').data();
   new Vue({
     el: '#job-page-app',
     components: {
