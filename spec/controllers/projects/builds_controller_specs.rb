34dd18d1a20af94f4b2aa2885453603742d8764d
Shinya Maeda <gitlab.shinyamaeda@gmail.com>
2017-04-17 19:59:55 +0800

    Add feature test with unicode trace
    Squashed commit of the following:
    
    commit 43e5bba774e9e383dd55c665e82f6fcfc4ebfc4f
    Author: Shinya Maeda <gitlab.shinyamaeda@gmail.com>
    Date:   Mon Apr 17 17:28:44 2017 +0900
    
        Add fix
    
    commit 09610eebdf22ad048812bb86022504b2ad917e19
    Author: Shinya Maeda <gitlab.shinyamaeda@gmail.com>
    Date:   Mon Apr 17 17:03:49 2017 +0900
    
        Add trace test

--- spec/controllers/projects/builds_controller_specs.rb
+++ spec/controllers/projects/builds_controller_specs.rb
@@ -3,38 +3,28 @@ require 'spec_helper'
 describe Projects::BuildsController do
   include ApiHelpers
 
+  let(:user) { create(:user) }
   let(:project) { create(:empty_project, :public) }
+  let(:pipeline) { create(:ci_pipeline, project: project) }
+
+  before do
+    sign_in(user)
+  end
 
   describe 'GET trace.json' do
-    let(:pipeline) { create(:ci_pipeline, project: project) }
     let(:build) { create(:ci_build, pipeline: pipeline) }
-    let(:user) { create(:user) }
 
-    context 'when user is logged in as developer' do
-      before do
-        project.add_developer(user)
-        sign_in(user)
-        get_trace
-      end
+    before do
+      allow_any_instance_of(Gitlab::Ci::Trace).to receive(:current_path).and_return(expand_fixture_path('trace/ansi-sequence-and-unicode'))
 
-      it 'traces build log' do
-        expect(response).to have_http_status(:ok)
-        expect(json_response['id']).to eq build.id
-        expect(json_response['status']).to eq build.status
-      end
+      get_trace
     end
 
-    context 'when user is logged in as non member' do
-      before do
-        sign_in(user)
-        get_trace
-      end
-
-      it 'traces build log' do
-        expect(response).to have_http_status(:ok)
-        expect(json_response['id']).to eq build.id
-        expect(json_response['status']).to eq build.status
-      end
+    it 'traces build log' do
+      expect(response).to have_http_status(:ok)
+      expect(json_response['id']).to eq build.id
+      expect(json_response['status']).to eq build.status
+      expect(json_response['html']).to include("ヾ(´༎ຶД༎ຶ`)ﾉ")
     end
 
     def get_trace
