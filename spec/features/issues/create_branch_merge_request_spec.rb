36c767145d7c57932a39d61f19271ebf08f36369
Alfredo Sumaran <alfredo@gitlab.com>
2017-04-28 03:05:09 -0500

    Fixes failing tests and consolidate feature tests into one file

--- spec/features/issues/create_branch_merge_request_spec.rb
+++ spec/features/issues/create_branch_merge_request_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 feature 'Create Branch/Merge Request Dropdown on issue page', feature: true, js: true do
   let(:user) { create(:user) }
-  let(:project) { create(:project_empty_repo) }
+  let!(:project) { create(:project) }
   let(:issue) { create(:issue, project: project, title: 'Cherry-Coloured Funk') }
 
-  context 'as an author' do
+  context 'for team members' do
     before do
       project.team << [user, :developer]
       login_as(user)
-      visit namespace_project_issue_path(project.namespace, project, issue)
     end
 
     it 'I can create a merge request from issue page' do
+      visit namespace_project_issue_path(project.namespace, project, issue)
+
       select_dropdown_option('create-mr')
 
       wait_for_ajax
@@ -27,6 +28,8 @@ feature 'Create Branch/Merge Request Dropdown on issue page', feature: true, js:
     end
 
     it 'I can creates a branch from issue page' do
+      visit namespace_project_issue_path(project.namespace, project, issue)
+
       select_dropdown_option('create-branch')
 
       wait_for_ajax
@@ -35,20 +38,54 @@ feature 'Create Branch/Merge Request Dropdown on issue page', feature: true, js:
       expect(current_path).to eq namespace_project_tree_path(project.namespace, project, '1-cherry-coloured-funk')
     end
 
-    def select_dropdown_option(option)
-      find('.create-mr-dropdown-wrap .dropdown-toggle').click
-      find("li[data-value='#{option}']").click
-      find('.js-create-merge-request').click
+    context "when there is a referenced merge request" do
+      let!(:note) do
+        create(:note, :on_issue, :system, project: project, noteable: issue,
+                                          note: "mentioned in #{referenced_mr.to_reference}")
+      end
+
+      let(:referenced_mr) do
+        create(:merge_request, :simple, source_project: project, target_project: project,
+                                        description: "Fixes #{issue.to_reference}", author: user)
+      end
+
+      before do
+        referenced_mr.cache_merge_request_closes_issues!(user)
+
+        visit namespace_project_issue_path(project.namespace, project, issue)
+      end
+
+      it 'disables the create branch button' do
+        expect(page).to have_css('.create-mr-dropdown-wrap .unavailable:not(.hide)')
+        expect(page).to have_css('.create-mr-dropdown-wrap .available.hide', visible: false)
+        expect(page).to have_content /1 Related Merge Request/
+      end
+    end
+
+    context 'when issue is confidential' do
+      it 'disables the create branch button' do
+        issue = create(:issue, :confidential, project: project)
+
+        visit namespace_project_issue_path(project.namespace, project, issue)
+
+        expect(page).not_to have_css('.create-mr-dropdown-wrap')
+      end
     end
   end
 
-  context 'logged out' do
+  context 'for visitors' do
     before do
       visit namespace_project_issue_path(project.namespace, project, issue)
     end
 
-    it 'does not have the dropdown' do
+    it 'shows no buttons' do
       expect(page).not_to have_selector('.create-mr-dropdown-wrap')
     end
+  end
+
+  def select_dropdown_option(option)
+    find('.create-mr-dropdown-wrap .dropdown-toggle').click
+    find("li[data-value='#{option}']").click
+    find('.js-create-merge-request').click
   end
 end
